23935acd4efa1f44fe8bc003818ef65a972c2099
Jonathan Sterling <jonsterling@me.com>
2011-01-12 15:04:24 -0800

    Cleaned up GHDockingTableHeaderViewController method order and parameter naming a little bit

--- Classes/GHDockingTableHeaderViewController.m
+++ Classes/GHDockingTableHeaderViewController.m
@@ -12,31 +12,34 @@
   return [[self alloc] initWithTableView:tableView headerView:headerView];
 }
 
-- (id)initWithTableView:(UITableView *)aTableView headerView:(id)aHeaderView {
+- (id)initWithTableView:(UITableView *)tableView headerView:(id)aHeaderView {
   self = [super init];
   if (self != nil) {
     headerView = [aHeaderView retain];
-    aTableView.tableHeaderView = headerView;
+    tableView.tableHeaderView = headerView;
   } return self;
-}
-
-- (void)scrollViewDidScroll:(UITableView *)scrollView {
-  if (scrollView.contentOffset.y < 0) {
-    [self.headerView removeFromSuperview]; 
-    [scrollView.backgroundView addSubview:self.headerView];
-    self.headerIsDocked = YES;
-  }
-  
-  else if (self.headerIsDocked) {
-    [self.headerView removeFromSuperview];
-    [scrollView addSubview:self.headerView];
-    self.headerIsDocked = NO;
-  }
 }
 
 - (void)dealloc {
   [headerView release];
   [super dealloc];
+}
+
+#pragma mark -
+#pragma mark UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UITableView *)tableView {
+  if (tableView.contentOffset.y < 0) {
+    [self.headerView removeFromSuperview]; 
+    [tableView.backgroundView addSubview:self.headerView];
+    self.headerIsDocked = YES;
+  }
+  
+  else if (self.headerIsDocked) {
+    [self.headerView removeFromSuperview];
+    [tableView addSubview:self.headerView];
+    self.headerIsDocked = NO;
+  }
 }
 
 @end
