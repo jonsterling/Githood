5a72380c5fc1055f06609d9ec14b37654aa7834e
Jonathan Sterling <jonsterling@me.com>
2011-01-11 16:24:26 -0800

    Removed comment blah-blah in Githood-main.m

--- Githood-main.m
+++ Githood-main.m
@@ -1,11 +1,3 @@
-//
-//  Githood-main.m
-//  Githood
-//
-//  Created by Jon on 1/6/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
